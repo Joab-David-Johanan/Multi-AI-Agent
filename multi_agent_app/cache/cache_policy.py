5614d367eecab2c558bf69b2308e2b652750e772
Joab David Johanan <joabkindle@gmail.com>
2026-03-03 05:45:50 +0100

    fix: correct crypto ttl

--- multi_agent_app/cache/cache_policy.py
+++ multi_agent_app/cache/cache_policy.py
@@ -1,7 +1,7 @@
 TTL_POLICY = {
     "weather": 600,  # 10 minutes
     "news": 1800,  # 30 minutes
-    "crypto": 520,  # 2 minutes
+    "crypto": 120,  # 2 minutes
     "stock": 120,  # 2 minutes
     "static": 2592000,  # 30 days
     "general": 604800,  # 7 days
