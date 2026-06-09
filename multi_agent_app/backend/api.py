137787ec051bdfe363e157f811ae7769cdb62536
Joab David Johanan <joabkindle@gmail.com>
2026-06-09 18:16:53 +0530

    chore: add render react frontend site to cors middleware

--- multi_agent_app/backend/api.py
+++ multi_agent_app/backend/api.py
@@ -45,6 +45,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "https://multi-agent-frontend-otlx.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
